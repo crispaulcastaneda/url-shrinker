169f08bd907cd1ce2b750a6fbf0a956eb59535e6
crispaulcastaneda <cristianpaul_c@yahoo.com>
2020-10-08 19:11:46 +0800

    setting-up-server

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
+
+mongoose.connect('mongodb:localhost/urlShortener', {
+    useNewUrlParse: true, useUnifiedTopology: true
+})
 
 app.set('view engine', 'ejs')
 
 app.get('/', (req, res) => {
     res.render('index')
+})
+
+app.post('/shortUrls', (req, res) => {
+
 })
 
 app.listen(process.env.PORT || 5000);
